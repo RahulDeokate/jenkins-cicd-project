f1d8806d85c55dbfa571d11ba2c3a8e19f7123f2
RahulDeokate <97500645+RahulDeokate@users.noreply.github.com>
2025-08-03 16:19:50 +0000

    nodegroup logic debuging

--- terraform/modules/nodegroup/main.tf
+++ terraform/modules/nodegroup/main.tf
@@ -12,13 +12,13 @@ resource "aws_iam_role" "eks_node_role" {
   })
 }
 
-resource "aws_iam_policy_attachment" "node_policies" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  ])
+# resource "aws_iam_policy_attachment" "node_policies" {
+#   for_each = toset([
+#     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+#     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+#     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   ])
 
-    role = aws_iam_role.eks_node_role.name
-    policy_arn = each.value
-}
+#     role = aws_iam_role.eks_node_role.name
+#     policy_arn = each.value
+# }
