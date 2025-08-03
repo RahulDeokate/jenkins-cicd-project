e1d2fd9b4e7c763f43935c6193af29efb557593d
RahulDeokate <97500645+RahulDeokate@users.noreply.github.com>
2025-08-03 16:17:53 +0000

    Writing node group infrastructure

---
+++ terraform/modules/nodegroup/main.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks_node_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+            Service = "ec2.amazonaws.com"
+        }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "node_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ])
+
+    role = aws_iam_role.eks_node_role.name
+    policy_arn = each.value
+}
